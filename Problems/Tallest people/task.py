ea5e7594bc109c7a88824f604b28e74102189542
randim <randim05@mail.ru>
2020-08-19 10:45:42 +0400

    test kommit in test braich 1 v2

--- Problems/Tallest people/task.py
+++ Problems/Tallest people/task.py
@@ -1,2 +1,15 @@
-def tallest_people():
-    pass
+def tallest_people(**arg):
+    c = sorted(dict(arg))
+    max = []
+    for i in c:
+        if not max:
+            max.append(i)
+        else:
+            if arg[i] > arg[max[0]]:
+                max = []
+                max.append(i)
+            elif arg[i] == arg[max[0]]:
+                max.append(i)
+
+    for u in max:
+        print(f'{u} : {arg[u]}')
